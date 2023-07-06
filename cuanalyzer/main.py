006fa0968f4522decf853fa380184dac88a238ba
Marco <91213598+marcobackup@users.noreply.github.com>
2023-07-06 12:49:36 +0200

    Initialized qt quick project structure

---
+++ cuanalyzer/main.py
@@ -0,0 +1,17 @@
+import os
+from pathlib import Path
+import sys
+
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import QQmlApplicationEngine
+
+
+if __name__ == '__main__':
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    engine.load(os.fspath(Path(__file__).resolve().parent / '))
+
+    if not engine.rootObjects():
+        sys.exit(-1)
+
+    sys.exit(app.exec_())
